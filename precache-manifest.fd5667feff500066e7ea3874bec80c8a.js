d336aefe6b49725d1e9ff1fad6d2f65f362dc99a
Osman Hussein <osman_990@hotmail.com>
2019-02-08 20:22:48 +0000

    Updates

---
+++ precache-manifest.fd5667feff500066e7ea3874bec80c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ozzi_website123/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "655410c0d4b265eee4da",
+    "url": "/ozzi_website123/static/js/runtime~main.655410c0.js"
+  },
+  {
+    "revision": "af33c45b9c70f228c966",
+    "url": "/ozzi_website123/static/js/main.af33c45b.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/ozzi_website123/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "af33c45b9c70f228c966",
+    "url": "/ozzi_website123/static/css/main.b8a85859.chunk.css"
+  },
+  {
+    "revision": "8034f88a66571dc93f968cb06ef23c51",
+    "url": "/ozzi_website123/index.html"
+  }
+];
